aed8259d2be4bc1f9e5c0b1a65a2344d6ca5dadb
Kushagra-Rai-Nova <72453116+Kushagra-Rai-Nova@users.noreply.github.com>
2022-03-29 23:22:38 +0530

    Add files via upload

---
+++ otp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+int main(){
+  string str="0123456789";
+  string otp; int n, len;
+  n=str.length();
+  cout<<"enter the length of OTP: ";
+  cin>>len;
+  srand(time(NULL)); // For different values each time we run the code
+  for(int i=1;i<len;i++){
+   otp.push_back(str[rand() % n]);
+  }
+   cout<<" the otp generated is : "<<otp<<endl;
+   return 0;
+}
